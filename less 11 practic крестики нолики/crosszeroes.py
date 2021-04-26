3a8d92b573041af60b8d38c1e5f2bbbd82898b85
Nurgle <axilles90@gmail.com>
2021-04-26 19:05:48 +0300

    добавляем логики в класс GameFieldView

--- less 11 practic крестики нолики/crosszeroes.py
+++ less 11 practic крестики нолики/crosszeroes.py
@@ -4,6 +4,7 @@ from enum import Enum
 
 import pygame
 
+CELL_SIZE = 50  # размер кристиков и ноликов
 
 class Cell(Enum):
     """
@@ -18,7 +19,10 @@ class Player:
     """
     Класс игрока, содержащий тип значков и имя
     """
-    pass
+
+    def __init__(self, name, cell_type):
+        self._name = name
+        self._cell_type = cell_type
 
 
 class GameField:
@@ -26,8 +30,28 @@ class GameField:
 
 
 class GameFieldView:
-    pass
+    """
+    виджет игрового поля, который отображает его на экране, а также выясняет место клика
+    """
 
+    def __init__(self, field_to_observe):
+        # загразить картинки значков клеток
+        # отобразить первичное сосотояние поля
+        self._field = field_to_observe
+        # размеры поля
+        self._height = field_to_observe.height * CELL_SIZE
+        self._width = field_to_observe.width * CELL_SIZE
+
+        pass
+
+    def draw(self):  # рисуем поле
+        pass
+
+    def check_coords_correct(self, x, y):  # проверяем правильность координат
+        return True  # TODO: self._height учесть
+
+    def get_coords(self, x, y):  # вычислить координаты
+        return (0, 0)  # TODO: реально вычислить
 
 class GameRoundManager:
     """
@@ -66,5 +90,5 @@ class GameWindow:
                     x, y = event.x, event.y  # сохроняем координаты клика
 
                     if self._field_widget.check_coords_correct(x, y):  # если координаты корретны
-                        i, j = self._field_widget(x, y)  # передаем их и вытаскиваем их в массиве
+                        i, j = self._field_widget.get_coords(x, y)  # передаем их и вытаскиваем их в массиве
                         self._game_manager.handle_click(i, j)  # передаем это событие гейм менеджеру
