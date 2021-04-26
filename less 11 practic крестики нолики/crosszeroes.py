61f2525b90037e0da6893111307920227f58aee7
Nurgle <axilles90@gmail.com>
2021-04-26 18:43:39 +0300

    добавляем логики в класс GameWindow

--- less 11 practic крестики нолики/crosszeroes.py
+++ less 11 practic крестики нолики/crosszeroes.py
@@ -2,6 +2,8 @@
 
 from enum import Enum
 
+import pygame
+
 
 class Cell(Enum):
     """
@@ -37,15 +39,32 @@ class GameRoundManager:
         self._current_player = 0
         self._field = GameField()  # создаем поле
 
-    def main_loop(self):  # главный цикл программы
-        while not game_over:
-            player = self._player[self._current_player]  # взять текущего игрока
+    def handle_click(self):
+        player = self._player[self._current_player]  # взять текущего игрока
+        pass
 
 
 class GameWindow:
     """
     содержит виджет поля, а также менежера игрорового раунда
     """
+
     def __init__(self):
         #  иницилизацпия pygame
-        pass
+        self._field_widget = GameFieldView  # вещи для создание поля
+        player1 = Player(Cell.CROSS)  # создаем игроков
+        player2 = Player(Cell.ZERO)  # создаем игроков
+        self._game_manager = GameRoundManager(player1, player2)  # создаем менедженра раундов
+
+    def main_loop(self):  # главный цикл программы
+        finished = False
+        while not finished:
+            for event in pygame.ge:
+                if event.type == pygame.QUIT:
+                    finished = True
+                elif event.type == pygame.MOUSEBUTTONDOWN:  # клик по полю
+                    x, y = event.x, event.y  # сохроняем координаты клика
+
+                    if self._field_widget.check_coords_correct(x, y):  # если координаты корретны
+                        i, j = self._field_widget(x, y)  # передаем их и вытаскиваем их в массиве
+                        self._game_manager.handle_click(i, j)  # передаем это событие гейм менеджеру
